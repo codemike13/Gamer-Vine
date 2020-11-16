3bc4ebeb4acd01b87ca9dd2b1b4d7515d8b91555
Anthony Michael Stephens <mike.stephens@codeimmersives.com>
2020-11-15 22:53:05 -0500

    Frame built for reviews page and added custom star rating system

---
+++ JS/stars.js
@@ -0,0 +1,13 @@
+modStars = (ele) => {
+    let stars = document.querySelectorAll('#stars > div')
+    let rating = ele.id
+    //Reset stars incase of decreasing star amount selection
+    for (let star of stars) {
+        star.style.backgroundColor = 'white'
+    }
+    //Add stars base on user selection
+    for (let i = 0; i < rating; i++) {
+        stars[i].style.backgroundColor = 'yellow'
+    }
+}
+
