e7b07be9971eaf311222ee650ca664f33c916c51
Anthony Michael Stephens <mike.stephens@codeimmersives.com>
2020-11-19 15:11:34 -0500

    Finished

--- JS/news.js
+++ JS/news.js
@@ -33,6 +33,11 @@ function newNews() {
     const new_img = document.createElement("IMG");
     new_img.setAttribute('src', uploadedImg)
 
+    const new_button = document.createElement('BUTTON')
+    new_button.setAttribute('class', 'delete')
+    new_button.setAttribute('onclick', 'remove(this)')
+    new_button.innerText = 'X'
+
     //From form fill for new post
     const heading = document.getElementById("heading").value;
     const sub_head = document.getElementById("subHeading").value;
@@ -47,6 +52,7 @@ function newNews() {
     new_link.innerText = "Click here!"
 
     //Append new inner fields
+
     new_heading_div.appendChild(new_heading)
 
     new_image_div.appendChild(new_img)
@@ -54,6 +60,7 @@ function newNews() {
 
     new_body_p.appendChild(new_link)
     //Append to parent
+    new_post.appendChild(new_button)
     new_post.appendChild(new_heading_div);
     new_post.appendChild(new_image_div);
     new_post.appendChild(new_body_p)
@@ -75,3 +82,12 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 
+remove = (ele) => {
+
+    // When the 'X' button is clicked lets find its "grand"parent by accessing parentElement twice.
+    let parent = ele.parentElement.parentElement
+
+    // Now that we have the grandparent we can remove the h1 that = (ele.parentElement)
+    // whose child (ele) is the button that called remove() function.
+    parent.removeChild(ele.parentElement)
+}
